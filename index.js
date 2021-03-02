7252f9e61af4aa34c352b11f3044625d07449277
Kunal <kdshah6593@gmail.com>
2021-03-02 15:46:34 -0500

    commit tree eca7dbc0ecbfa730955973747c749b01023a3539

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) { 
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) { 
+    let regex = /[^\w\s\'\-]/g
+    return str.replace(regex, '')
+   }
+
+  static titleize(str) { 
+    let noCapital = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let strArray = str.split(" ");
+    strArray[0] = this.capitalize(strArray[0])
+
+    let newStr = strArray.map(e => {
+      if (!noCapital.includes(e)) {
+        return this.capitalize(e)
+      } else {
+        return e
+      }
+    })
+
+    return newStr.join(" ")
+  }
+
 }
